12118f8458c7be48bc08a624c40697a243e5a16e
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2023-10-17 14:15:12 +0900

    feat : update gitignore

--- backend/estagram/settings.py
+++ backend/estagram/settings.py
@@ -49,6 +49,12 @@ INSTALLED_APPS = [
     'story',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
