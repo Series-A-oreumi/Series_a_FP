4481f0a9804d4065655a3a6c2474d7a391f04a03
ansoyun12 <dksthdbs43@gmail.com>
2023-10-23 17:31:48 +0900

    feat:add header,sidebar

---
+++ frontend/js/sidebar.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+
+    // 'sidebarHidden' 클래스를 토글하여 나타났다가 숨겨진 상태로 전환합니다.
+    sidebar.classList.toggle("sidebarHidden");
+}
