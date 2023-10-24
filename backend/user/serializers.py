24eb0beb5edb9fb8a1d0c0a46af7c46e8bc12b9b
minsuhaha <haminsu5@gmail.com>
2023-10-24 09:03:16 +0900

    feat : add unique validation

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth import authenticate
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,12 +21,23 @@ class LoginSerializer(serializers.ModelSerializer):
             user = UserProfile.objects.filter(email=email).first()
             if user:
                 if user.password == password:
-                    return user
+                    token_data = user.get_token()
+                    refresh = token_data['refresh']
+                    access = token_data['access']
+
+                    data = {
+                        'user': user,
+                        'refresh': refresh,
+                        'access': access,
+                    }
+
+                    return data
                 else:
                     raise serializers.ValidationError("비밀번호가 일치하지 않습니다.")
             else:
                 raise serializers.ValidationError("사용자가 존재하지 않습니다.")
 
+        
     class Meta:
         model = UserProfile
         fields = ["email", "password"]
@@ -37,15 +50,39 @@ class RegistrationSerializer(serializers.ModelSerializer):
         ('AI 1기', 'AI WASSUP 1기'),
     )
 
-    email = serializers.EmailField(min_length=8, write_only=True)
-    username = serializers.CharField(max_length=30)
-    password = serializers.CharField(min_length=8, write_only=True)
-    password2 = serializers.CharField(min_length=8, write_only=True)
-    nickname = serializers.CharField(max_length=255)
-    bootcamp = serializers.ChoiceField(choices=BOOTCAMP_CHOICES)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=UserProfile.objects.all())],
+    )
+    username = serializers.CharField(
+        max_length=30, 
+        required=True
+        )
+    password = serializers.CharField(
+        min_length=8, 
+        write_only=True, 
+        required=True,
+        )
+    password2 = serializers.CharField(
+        min_length=8, 
+        write_only=True, 
+        required=True)
+    nickname = serializers.CharField(
+        max_length=255, 
+        validators=[UniqueValidator(queryset=UserProfile.objects.all())]
+        )
+    bootcamp = serializers.ChoiceField(
+        required=True,
+        choices=BOOTCAMP_CHOICES
+        )
 
+
+    class Meta:
+        model = UserProfile
+        fields = ("email", "password", "password2", "username", "nickname", "bootcamp")
+    
     def create(self, validated_data):
-        validated_data.pop("password2")
+        validated_data.pop("password2") # UserProfile 모델에 password2라는 필드가 따로 없고 확인용이기에 pop
         user = UserProfile.objects.create(**validated_data)
         return user
 
@@ -60,6 +97,4 @@ class RegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Passwords must match")
         return attrs
 
-    class Meta:
-        model = UserProfile
-        fields = ("email", "password", "password2","username", "nickname", "bootcamp")
+   
