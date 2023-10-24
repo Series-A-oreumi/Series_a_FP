f3dac3a56301fed5a137ac68f90d1f86f9107a43
minsuhaha <haminsu5@gmail.com>
2023-10-24 09:01:09 +0900

    feat : update jwt-token url

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from .views import *
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+
+    # jwt-token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # 토큰 재발급을 위한 url
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
