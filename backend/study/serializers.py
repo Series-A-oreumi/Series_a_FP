0c0257a0ae135bc14289e5ec2fbe9ab427b2dcab
minsuhaha <haminsu5@gmail.com>
2023-10-25 23:00:56 +0900

    feat : update <story&comment serializer>

--- backend/study/serializers.py
+++ backend/study/serializers.py
@@ -13,12 +13,35 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
 
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+# studylist
 class StudySerializer(serializers.ModelSerializer):
     author = UserProfileSerializer(read_only=True) # 스터디 게시한 사람
+    stacks = StackSerializer(many=True, read_only=True) # 해당 스터디 기술 스택
+    likes_users = serializers.SerializerMethodField() # 좋아요를 누른 유저 목록
+
+    class Meta:
+        model = Study
+        fields = ('author', 'title', 'content', 'end_at', 'views', 'comments_count', 'project_study', 
+                  'likes_users', 'online_offline', 'field', 'stacks', 'public_private')
+
+    def get_likes_users(self, post):
+        # post는 현재 직렬화되는 Post 인스턴스
+        likes_users = post.likes.all()
+        return [user.username for user in likes_users] # 좋아요를 누른 유저들의 정보를 시리얼라이즈하여 반환.
+
+# studydetail
+class StudyDetailSerializer(serializers.ModelSerializer):
+    author = UserProfileSerializer(read_only=True) # 스터디 게시할 사람
     likes_users = serializers.SerializerMethodField(read_only=True) # 좋아요를 누른 유저 목록을 가져올 필드를 추가합니다.
     stacks = StackSerializer(many=True, read_only=True) # 해당 스터디 기술 스택
-    participant_users = serializers.SerializerMethodField(read_only=True) # 스터디 및 프로젝트 참여자 목록
-    comments = serializers.SerializerMethodField(read_only=True) # 해당 스터디에 달린 댓글들
+    participant_users = serializers.SerializerMethodField(read_only=True) # 스터디 및 프로젝트 참여자 목록 (리스트)
+    comments_count = serializers.SerializerMethodField(read_only=True) # 해당 스터디에 달린 댓글수
+    comments_list = serializers.SerializerMethodField(read_only=True) # 해당 스터디에 달린 댓글 리스트
 
     class Meta:
         model = Study
@@ -38,7 +61,23 @@ class StudySerializer(serializers.ModelSerializer):
         # 참여자 유저들의 정보를 시리얼라이즈하여 반환.
         return [user.username for user in participant_users]
     
-    def get_comments(self, study):
-        comments = study.comments_study.all()
-        return comments
+    def get_comments_count(self, study):
+        comment_count = study.comments_study.all().count()
+        return comment_count
     
+    def get_comments_list(self, study):
+        comments = study.comments_study.all() 
+
+        # comments는 queryset 형태이기 때문에 직렬화 작업 해주기
+        serialized_comments = CommentSerializer(comments, many=True).data 
+        return serialized_comments
+    
+    class Meta:
+        model = Study
+        fields = '__all__'
+
+# studycreate
+class StudyCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Study
+        fields = '__all__'
