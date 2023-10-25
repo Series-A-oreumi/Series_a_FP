eec8bb33dd9e00ac2c24ded6a77dbb40ccce2d51
minsuhaha <haminsu5@gmail.com>
2023-10-25 23:01:15 +0900

    feat : add <story&comment url>

--- backend/study/urls.py
+++ backend/study/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # study 
     path('', views.StudyList.as_view()), # 스터디 목록
     path('create/', views.StudyCreate.as_view()), # 스터디 생성
+    path('<int:study_id>/', views.StudyDetail.as_view()), # 스터디 상세
+
+    # comment
+    path('<int:study_id>/comments', views.CommentCreate.as_view()), # 댓글 작성
+
 ]
