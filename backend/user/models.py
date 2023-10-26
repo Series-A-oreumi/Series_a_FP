1b55c616ad6ea1bf3e2a31abec9189fcf8d65ce1
minsuhaha <haminsu5@gmail.com>
2023-10-26 17:35:50 +0900

    feat : update <Userprofile model field>

--- backend/user/models.py
+++ backend/user/models.py
@@ -7,6 +7,7 @@ class UserProfile(models.Model):
     password = models.CharField(max_length=255)
     nickname = models.CharField(max_length=255)
     bootcamp = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nickname
@@ -17,3 +18,9 @@ class UserProfile(models.Model):
             "refresh": str(refresh),
             "access": str(refresh.access_token),
         }
+    
+# class Profile(models.Model):
+#     user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+#     profile_img = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+#     about = models.TextField(null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
