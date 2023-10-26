54868c6dd38ae4c6d7bb4c7156db0f7e80484f51
minsuhaha <haminsu5@gmail.com>
2023-10-26 13:18:09 +0900

    feat : add <comment url>

--- backend/study/urls.py
+++ backend/study/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     # study 
     path('', views.StudyList.as_view()), # 스터디 목록
     path('create/', views.StudyCreate.as_view()), # 스터디 생성
-    path('<int:study_id>/', views.StudyDetail.as_view()), # 스터디 상세
+    path('<int:study_id>/', views.StudyDetail.as_view()), # 스터디 상세 & 수정 & 삭제
 
     # comment
-    path('<int:study_id>/comments', views.CommentCreate.as_view()), # 댓글 작성
+    path('<int:study_id>/comments/', views.CommentCreate.as_view()), # 댓글 작성
+    path('comments/<int:comment_id>/', views.CommentUpdateDelete.as_view()), # 댓글 수정 & 삭제
 
 ]
