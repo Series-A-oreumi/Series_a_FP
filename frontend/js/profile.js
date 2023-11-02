341d16ad1d10f3c7b91ba4a7b092da45e71e60a6
wjsdlsghk12 <wjsdlsghk12@naver.com>
2023-11-02 18:28:02 +0900

    chat_fin

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -57,11 +57,12 @@ document.addEventListener("DOMContentLoaded", async function () {
         // 프로필
         function creatProfile(data, story, study) {
             const chatUrl = `../html/chat.html?data=${data.nickname}`
+            console.log(data.profile_img)
 
             return `
             <div class="up-profile">
                 <div class="up-profile-img">
-                    <img src="${data.profile_img}.url" alt="" class="up-img">
+                    <img src="${data.profile_img}" alt="" class="up-img">
                 </div>
                 <div class="up-detail-profile">
                     <div class="up-profile-name">
