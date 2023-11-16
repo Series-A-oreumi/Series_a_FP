389cae007ffa22046329b91b58e23f319e0799b6
minsuhaha <haminsu5@gmail.com>
2023-11-16 16:31:19 +0900

    feat : 알람 수정 취소

--- backend/alrarm/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Notification(models.Model):
-    content = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
