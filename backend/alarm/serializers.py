22413ba1ac4ab7db7ebc5d59020eb89d67ccd0df
minsuhaha <haminsu5@gmail.com>
2023-11-17 11:17:01 +0900

    feat : alarmserializer team 필드 추가생성

--- backend/alarm/serializers.py
+++ backend/alarm/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 from .models import Alarm
+from study.models import Team
 
 
 class AlarmSerializer(serializers.ModelSerializer):
-    
+    team = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Alarm
         fields = '__all__'
+
+    def get_team(self, alarm):
+        study = alarm.study
+        if study:
+            try:
+                team = Team.objects.get(study=study)
+                return team.id
+            except Team.DoesNotExist:
+                return None
+        return None
+    
