6f07d9eec70380dc885fa5ff7e0717becfd4b663
minsuhaha <haminsu5@gmail.com>
2023-12-02 19:58:12 +0900

    feat : alarm serializer에서 team 필드 메소드 제거

--- backend/alarm/serializers.py
+++ backend/alarm/serializers.py
@@ -4,21 +4,10 @@ from study.models import Team
 
 
 class AlarmSerializer(serializers.ModelSerializer):
-    team = serializers.SerializerMethodField(read_only=True)
     sender = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Alarm
         fields = '__all__'
-
-    def get_team(self, alarm):
-        study = alarm.study
-        if study:
-            try:
-                team = Team.objects.get(study=study)
-                return team.id
-            except Team.DoesNotExist:
-                return None
-        return None
     
     def get_sender(self, alarm):
         sender = alarm.sender.nickname
