f2803ddec1729500680f9cf0e7104e64113c5e1f
minsuhaha <haminsu5@gmail.com>
2023-12-07 13:41:47 +0900

    feat : 운영진 권한을 위한 permission 생성

--- backend/user/permissions.py
+++ backend/user/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission
 from rest_framework_simplejwt.tokens import AccessToken
+from .models import UserProfile
 
 class IsTokenValid(BasePermission):
     def has_permission(self, request, view):
@@ -8,5 +9,17 @@ class IsTokenValid(BasePermission):
             access_token = AccessToken(token)
             user = access_token.payload.get('user_id')
             return user is not None
+        except Exception as e:
+            return False
+
+class IsAdminValid(BasePermission):
+    def has_permission(self, request, view):
+        try:
+            token = request.META.get('HTTP_AUTHORIZATION', '').split(' ')[1]  # JWT 토큰 추출
+            access_token = AccessToken(token)
+            user_id = access_token.payload.get('user_id')
+            user = UserProfile.objects.get(pk=user_id)
+            
+            return user.is_active and user.is_member == 'admin'
         except Exception as e:
             return False
